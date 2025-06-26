16f9048fec42e4906c69b3231c86faa5ceda3322
paarth-r <paarth.rajpal@gmail.com>
2025-06-26 14:22:05 -0700

    new post and grammar fix

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Page() {
       </h1>
       <p className="mb-4">
         {`I’m a student developer exploring computer vision, robotics, and AI — and their intersection. 
-        I spend a lot of my time building trying to solve real-world problems through research and SOTA technologies
-        — whether it’s improving how robots see the world, or using CV and AI to make fitness more accessible.
+        I spend a lot of my time building, trying to solve real-world problems through research and SOTA technologies
+        —whether it’s improving how robots see the world, or using CV and AI to make fitness more accessible.
         I’m always experimenting, learning, and striving to turn ideas into things that actually work.`}
       </p>
       <div className="my-8">
